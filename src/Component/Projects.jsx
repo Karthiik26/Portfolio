4d3ad3da8cd809d7e139b0276fb1201d739f1a02
Karthiik26 <imkarthiik26@gmail.com>
2024-07-16 20:14:11 +0530

    proj-image change

--- src/Component/Projects.jsx
+++ src/Component/Projects.jsx
@@ -5,10 +5,8 @@ import { IoCall } from "react-icons/io5";
 import Aos from "aos";
 import "aos/dist/aos.css";
 import { useNavigate } from "react-router-dom";
-import Todoimg from '../assets/image2.png'
-import Lectureimg from '../assets/image.png'
-import TailorImg from '../assets/tailor1.png'
-import EcomImg from '../assets/image3.png'
+import Todoimg from "../assets/image2.png";
+import Lectureimg from "../assets/image.png";
 
 const Projects = () => {
 
@@ -158,7 +156,7 @@ const Projects = () => {
                 rel="noopener noreferrer"
               >
                 <img
-                  src={TailorImg}
+                  src="../assets/tailor1.png"
                   alt="Img Not Found"
                   width={600}
                   className="border-2 border-gray-800 rounded-lg"
@@ -209,7 +207,7 @@ const Projects = () => {
           <div data-aos-duration="2000" data-aos="fade-up">
             <div className="w-auto my-4 py-6 px-4 shadow-xl shadow-gray-400 rounded-lg h-[500px] animate-flyinBottom">
               <img
-                src={EcomImg}
+                src="../assets/image3.png"
                 alt="Img Not Found"
                 width={600}
                 className="border-2 border-gray-800 rounded-lg"
